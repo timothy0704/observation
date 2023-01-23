bc53377b31e6fe57359b0997d3ce5c6b0f8c316b
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-01-23 17:26:08 -0500

    Create number12.cs

---
+++ number12.cs
@@ -0,0 +1,13 @@
+using System;  // math function
+
+class Program {
+  public static void Main (string[] args) {
+
+int x=10, y=2;
+    double num=2.56;
+  double z=Math.Pow(x,y);
+    
+    Console.WriteLine(Math.Round(num,1));
+    
+  }
+}  
