c84216ac71eebcae93c983db222435375e0842d2
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-01-23 17:29:04 -0500

    Create nuber9.cs

---
+++ nuber9.cs
@@ -0,0 +1,17 @@
+using System;  //number 4
+
+class Program {
+  public static void Main (string[] args) {
+
+int n= 5;
+    for (int i=1; i <= n; i++) 
+    {
+      for (int j=1; j <= i; j++)
+      {
+        Console.Write(j);
+      }
+      Console.WriteLine();
+    }
+    
+  }
+}   
