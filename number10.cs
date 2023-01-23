99eb5798d78c05a6733024e5cfab8ea51969cfe3
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-01-23 17:52:45 -0500

    Create number10.cs

---
+++ number10.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    
+   int sum = 0;
+    int i = 1;
+
+    while(i<=99){
+
+      sum+=i;
+      i+=2;
+      
+    }  
+    Console.WriteLine(sum);
+  }
+}
