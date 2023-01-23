c5f22b0ed7d6187213ed2bc37e65b24b48c15028
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-01-23 17:05:08 -0500

    Create number3.cs

---
+++ number3.cs
@@ -0,0 +1,59 @@
+/*using System;   // number 1 in for statement 
+
+class Program {
+  public static void Main (string[] args) {
+
+    int sum = 0;
+    
+  for(int n = 0; n < 51; n++){
+    sum += n;
+  }
+  Console.WriteLine(sum);
+  }
+}   */
+
+
+
+/*using System;     //number 1 in while statement 
+
+class Program {
+  public static void Main (string[] args) {
+
+    int sum = 0;
+  int n=1;
+    while(n<51){
+  
+    
+      sum += n;
+    
+      n++;
+      
+   }
+Console.WriteLine(sum);
+  
+  }
+}  */
+
+
+
+
+/*using System;     //number 1 in do while statement 
+
+class Program {
+  public static void Main (string[] args) {
+
+    int sum = 0;
+    int n=1;
+
+do{
+
+  sum += n;
+    
+      n++;
+} while(n<51);
+
+    Console.WriteLine(sum);
+  
+  
+  }
+}   */
