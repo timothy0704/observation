57799457dcc700456e5f897a18332ff7527b40c3
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2023-01-23 17:16:20 -0500

    Create number5.cs

---
+++ number5.cs
@@ -0,0 +1,56 @@
+/*using System;   // number 3 in for statement 
+
+class Program {
+  public static void Main (string[] args) {
+
+    int sum = 0;
+    
+  for(int n = 25; n<75; n++){
+    sum += n;
+  }
+  Console.WriteLine(sum);
+  }
+} */
+
+
+/*using System;     //number 3 in while statement 
+
+class Program {
+  public static void Main (string[] args) {
+
+    int sum = 0;
+    int n=25;
+    while(n<75){
+  
+    
+      sum += n;
+    
+      n++;
+      
+   }
+Console.WriteLine(sum);
+  
+  }
+} */
+
+
+/*using System;     //number 1 in do while statement 
+
+class Program {
+  public static void Main (string[] args) {
+
+    int sum = 0;
+    int n=25;
+
+do{
+
+  sum += n;
+    
+      n++;
+} while(n<75);
+
+    Console.WriteLine(sum);
+  
+  
+  }
+} */
